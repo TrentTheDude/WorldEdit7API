6fe4ecb49fc99da2ffbb22b55e5b6cb1a90fee6f
Trent <trent.wallert@gmail.com>
2019-07-09 19:51:16 -0500

    Init2

---
+++ src/main/java/me/trent/worldedit7/WorldEdit7.java
@@ -0,0 +1,61 @@
+package me.trent.worldedit7;
+
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class WorldEdit7 {
+
+    public static Region getSelectionRegion(Player p) {
+        try {
+            return getWorldEdit().getSession(p).getSelection(new BukkitWorld(p.getWorld()));
+        }catch(IncompleteRegionException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static WorldEditPlugin getWorldEdit() {
+        Plugin p = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+        if (p instanceof WorldEditPlugin) return (WorldEditPlugin) p;
+        else return null;
+    }
+
+    public static void paste(File file, Location location){
+        ClipboardFormat format = ClipboardFormats.findByFile(file);
+        try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
+            Clipboard clipboard = reader.read();
+            try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(location.getWorld()), -1)) {
+                Operation operation = new ClipboardHolder(clipboard)
+                        .createPaste(editSession)
+                        .to(BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ()))
+                        .ignoreAirBlocks(false)
+                        .build();
+                Operations.complete(operation);
+            }
+        } catch (IOException | WorldEditException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+}
